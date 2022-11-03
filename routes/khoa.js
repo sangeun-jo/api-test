8eceac49b6acec551e5cdcc9db7472b2f7a0a2dd
sangeun jo <sangeun.jo@outlook.kr>
2022-11-03 11:46:47 +0900

    first

---
+++ routes/khoa.js
@@ -0,0 +1,11 @@
+//국립 해양 조사원 개방해 API
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('khoa', );
+});
+
+
+module.exports = router;
